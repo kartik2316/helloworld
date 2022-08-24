a36d5597c4639995882c2ed4c4cb73cbd9e11777
kartikeya <kartikeyasingh.ks43@gmail.com>
2022-08-24 23:56:36 +0530

    first java commit

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
-class hello{  
+public class hello{  
     public static void main(String args[]){  
-     System.out.println(89); 
+     System.out.println(89);
+     System.out.println("hiiiiiiiiiiiii");
     }  
 }  
